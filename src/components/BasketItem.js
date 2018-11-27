27b0a4f03b1bea1da0ecc509e1d907c9c939370f
kyun <lsk9105@gmail.com>
2018-11-27 10:29:46 +0900

    Food court 초기 뷰

--- src/components/BasketItem.js
+++ src/components/BasketItem.js
@@ -5,7 +5,11 @@ const BasketItem = ({ name, price, count }) => (
   <div className="BasketItem">
     <div className="name">{name}</div>
     <div className="price">{price}원</div>
-    <div className="count">{count}</div>
+    <div className="count">
+      <button>+</button>
+      {count}
+      <button>-</button>
+    </div>
     <div className="return">갖다놓기</div>
   </div>
 );
---
+++ src/modules/basket.js
@@ -0,0 +1,6 @@
+import produce from 'immer';
+
+const INSERT = 'basket/INSERT';
+const INCREMENT = 'basket/INCREMENT';
+const DECREMENT = 'basket/DECREMENT';
+const REMOVE = 'basket/REMOVE';
